a0771f5947b7b32324ca09401d6d3fbf1d5bf7d5
jjp32 <jjp32>
2001-01-30 07:18:42 +0000

    Sienaified

--- EventNotifier.java
+++ EventNotifier.java
@@ -1,20 +1,26 @@
 package psl.xues;
 
-import psl.groupspace.*;
+import siena.*;
 import java.io.*;
 import java.util.*;
 
 /**
  * EventNotifier for Xues.
  *
+ * Sorry for sparse comments, this version is being written at 2:09 AM.
+ *
  * Copyright (c) 2000: The Trustees of Columbia University and the
  * City of New York.  All Rights Reserved.
  *
  * @author Janak J Parekh
- * @version 0.01 (9/7/2000)
+ * @version 0.9
  *
  * $Log$
- * Revision 1.3  2000-12-26 22:25:13  jjp32
+ * Revision 1.4  2001-01-30 07:18:42  jjp32
+ *
+ * Sienaified
+ *
+ * Revision 1.3  2000/12/26 22:25:13  jjp32
  *
  * Updating to latest preview versions
  *
@@ -27,37 +33,48 @@ import java.util.*;
  * Added EventNotifier code; updated previous event code
  *
  */
-public class EventNotifier implements GroupspaceService,
-				      GroupspaceCallback {
-  GroupspaceController gcRef = null;
-  private String roleName = "EventNotifier";
+public class EventNotifier implements Notifiable {
+  protected static String sienaHost = null;
+  protected Siena sienaRef = null;
+
+  public static boolean DEBUG = false;
+
+  public static void main(String[] args) {
+    if(args.length < 1) { // Siena host specified?
+      usage();
+    }
+
+    for(int i=0; i < args.length; i++) {
+      if(args[i].equals("-s"))
+	sienaHost = args[++i];
+      else if(args[i].equals("-d"))
+	DEBUG = true;
+      else
+	usage();
+    }
+
+    new EventNotifier();
+  }
+
+  /**
+   * Print usage.
+   */
+  public static void usage() {
+    System.out.println("usage: java EventNotifier [-s sienaHost] [-d] [-?]");
+    System.exit(-1);
+  }
+
 
   public EventNotifier() { 
     // Nothing right now
   }
 
-  public boolean gsInit(GroupspaceController gc) {
-    this.gcRef = gc;
-    this.gcRef.registerRole(roleName, this);
-    // Subscribe to EventPackager events
-    this.gcRef.subscribeEvent(this,"EventNotifierIncoming");
-    return true;
+  public void notify(Notification n) {
+    ;
   }
 
-  public void gsUnload() {
-    // Nothing right now 
+  public void notify(Notification[] n) {
+    ;
   }
-
-  public void run() { 
-  }
-
-  public int callback(GroupspaceEvent ge) {
-    // Received event, talk to TriKX
-
-    return GroupspaceCallback.CONTINUE;
-
-  }
-
-  public String roleName() { return this.roleName; }
     
 }
