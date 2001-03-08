943eeb024618918b677ef5eab6a5b29cfe441163
jjp32 <jjp32>
2001-03-08 01:29:15 +0000

    Oops, forgot to commit these

--- EDTest.java
+++ EDTest.java
@@ -5,42 +5,51 @@ import java.net.*;
 import java.util.*;
 import siena.*;
 
-/** Generates ED-like XML notification.
-  * One argument: xml message to include.
-  *
-  * $Log$
-  * Revision 1.3  2001-01-30 06:26:18  jjp32
-  *
-  * Lots and lots of updates.  EventDistiller is now of demo-quality.
-  *
-  * Revision 1.2  2001/01/30 02:39:36  jjp32
-  *
-  * Added loopback functionality so hopefully internal siena gets the msgs
-  * back
-  *
-  * Revision 1.1  2001/01/30 00:24:50  jjp32
-  *
-  * Bug fixes, added test class
-  *
-  * Revision 2.2  2001/01/29 04:04:48  png3
-  * Added package psl.metaparser statements.  Can you say "Oops?"
-  *
-  * Revision 2.1  2001/01/28 17:52:17  png3
-  * New version of Metaparser: fully multithreaded.  PrintWriter logs.
-  *
-  */
-public class SienaTest {
+/**
+ * Generates ED-like XML notification.
+ * @author Janak J Parekh
+ * @version 0.1
+ *
+ * $Log$
+ * Revision 1.1  2001-03-08 01:29:15  jjp32
+ * Oops, forgot to commit these
+ *
+ * Revision 1.3  2001/01/30 06:26:18  jjp32
+ *
+ * Lots and lots of updates.  EventDistiller is now of demo-quality.
+ *
+ * Revision 1.2  2001/01/30 02:39:36  jjp32
+ *
+ * Added loopback functionality so hopefully internal siena gets the msgs
+ * back
+ *
+ * Revision 1.1  2001/01/30 00:24:50  jjp32
+ *
+ * Bug fixes, added test class
+ *
+ * Revision 2.2  2001/01/29 04:04:48  png3
+ * Added package psl.metaparser statements.  Can you say "Oops?"
+ *
+ * Revision 2.1  2001/01/28 17:52:17  png3
+ * New version of Metaparser: fully multithreaded.  PrintWriter logs.
+ *
+ */
+public class EDTest {
   
   /** Constructor */
   public static void main(String[] args) {
     
     String xml = null;
-    
+
+    if(args.length == 0 || args[0].equals("-?")) {
+      System.out.println("Usage: java EDTest <senp url>");
+      System.exit(0);
+    }
     
     HierarchicalDispatcher hd = new HierarchicalDispatcher();
     try {
       hd.setReceiver(new TCPPacketReceiver(61981));
-      hd.setMaster("senp://canal.psl.cs.columbia.edu:4321");
+      hd.setMaster(args[0]);
     } catch (InvalidSenderException ise) {
       System.err.println("Invalid Sender:" + ise);
       return;
